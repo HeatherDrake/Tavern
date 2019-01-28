ea58edf5806087dec8c75a1161978f6fc346ff30
Heather Drake <heatherdrake@heathers-mbp.home>
2019-01-27 20:23:31 -0500

    added app files

--- conf/urls.py
+++ conf/urls.py
@@ -1,5 +1,4 @@
 """conf URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
@@ -12,10 +11,24 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# """
+# from django.contrib import admin
+# from django.urls import path
+# from tavern.views import Home, Details, Results, Vote
+#
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', Home.as_view()),
+#     path('', Vote.as_view()),
+#     path('', Results.as_view()),
+#     path('', Details.as_view()),
+# ]
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+   path('admin/', admin.site.urls),
+   path('', include('tavern.urls')),
 ]
--- tavern/models.py
+++ tavern/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Lunch(models.Model):
+    nickname = models.CharField(max_length = 150)
+    user = models.CharField(max_length = 150)
+    date = models.CharField(max_length = 150)
+
+
+
+    def __str__(self):
+        return self.nickname
+
+
+
+class Location(models.Model):
+    lunch = models.ForeignKey('Lunch', on_delete=models.CASCADE)
+    suggested_locations = models.CharField(max_length=100,  null=True)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.suggested_locations
